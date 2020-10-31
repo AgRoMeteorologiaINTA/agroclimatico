4aa98a0ea5b4b5a99aa2eb99c9a0c9ac0ac219ec
Elio Campitelli <eliocampitelli@gmail.com>
2020-10-31 18:47:26 -0300

    Cambia nombre de argumento de capacidad de campo

--- R/pdsi.R
+++ R/pdsi.R
@@ -4,18 +4,18 @@
 #'
 #' @param precipitacion Serie de precipitación sin datos faltantes.
 #' @param etp Serie de evapotranspiración potencial sin datos faltantes.
-#' @param cad Cantidad de Agua Disponible (en mm).
+#' @param cc Capacidad de campo (en mm).
 #
 #
 #' @export
-pdsi  <- function(precipitacion, etp, cad = 100) {
-  as.vector(scPDSI::pdsi(precipitacion, etp, AWC = cad, sc = FALSE)$X)[seq_along(precipitacion)]
+pdsi  <- function(precipitacion, etp, cc = 100) {
+  as.vector(scPDSI::pdsi(precipitacion, etp, AWC = cc, sc = FALSE)$X)[seq_along(precipitacion)]
 }
 
 
 
 #' @export
 #' @rdname pdsi
-pdsi_ac  <- function(precipitacion, etp, cad = 100) {
-  as.vector(scPDSI::pdsi(precipitacion, etp, AWC = cad, sc = TRUE)$X)[seq_along(precipitacion)]
+pdsi_ac  <- function(precipitacion, etp, cc = 100) {
+  as.vector(scPDSI::pdsi(precipitacion, etp, AWC = cc, sc = TRUE)$X)[seq_along(precipitacion)]
 }
