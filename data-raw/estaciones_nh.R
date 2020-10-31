7aae7961815c90e4f70142e47d3ad91026cfc5a5
Elio Campitelli <eliocampitelli@gmail.com>
2020-10-31 18:48:38 -0300

    Documenta todo

--- data-raw/estaciones_nh.R
+++ data-raw/estaciones_nh.R
@@ -3,7 +3,7 @@
 col_metadatos_nh <- c("n", "codigo_nh", "ext", "nombre", "lat", "lon", "tmp")
 
 estaciones_nh <- readr::read_fwf(file = "data-raw/LISTA.DAT",
-                         fwf_widths(widths = c(2, 4, 5, 31, 9, 9, 7),
+                         readr::fwf_widths(widths = c(2, 4, 5, 31, 9, 9, 7),
                                     col_names = col_metadatos_nh)) %>%
   dplyr::select(codigo_nh, nombre, lat, lon)
 
