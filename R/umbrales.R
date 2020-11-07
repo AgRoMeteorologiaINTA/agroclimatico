2945b393c46ccf6945808e3344765b6efcdf55c3
Elio Campitelli <eliocampitelli@gmail.com>
2020-11-07 20:22:13 -0300

    Mejora umbrales

--- R/umbrales.R
+++ R/umbrales.R
@@ -5,16 +5,27 @@
 #' @export
 umbrales <- function(...) {
   values <- list(...)
+  if (length(values) == 0) {
+    stop("Ningún extremo definido.")
+  }
 
   datos <- lapply(values, function(x) {
-
+    prop <- mean(x, na.rm = TRUE)
+    prop <- ifelse(is.finite(prop), prop, NA_real_)
     data.frame(N = sum(x, na.rm = TRUE),
-               prop = mean(x, na.rm = TRUE),
+               prop = prop,
                na = mean(is.na(x)))
 
   })
   datos <- do.call(rbind, datos)
-  datos$extremo <- names(values)
+
+  if (is.null(names(values))) {
+    names <- paste0("V", seq_along(values))
+    warning("Los argumentos no tienen nombre asignando nombres: ", paste0(names, collapse = ", "), ".")
+  } else {
+    names <- names(values)
+  }
+  datos$extremo <- names
   rownames(datos) <- NULL
   datos
 }
