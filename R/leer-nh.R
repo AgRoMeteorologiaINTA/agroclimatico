441029b902bbc7b101d2eb4b068df712d53ea8cb
Elio Campitelli <eliocampitelli@gmail.com>
2020-11-07 20:20:40 -0300

    Agrega el theme de INTA al mapa de estaciones

--- R/leer-nh.R
+++ R/leer-nh.R
@@ -81,7 +81,9 @@ plot.metadatos_nh <- function(x, ...) {
   }
 
   ggplot2::ggplot(x, ggplot2::aes(lon, lat)) +
-    ggplot2::geom_sf(data = mapa_argentina_provincias, inherit.aes = FALSE) +
-    ggplot2::geom_point()
+    ggplot2::geom_sf(data = mapa_provincias(), inherit.aes = FALSE) +
+    ggplot2::geom_point() +
+    theme_inta_mapa() +
+    coord_argentina()
 }
 
