724141592514d448c0ad067f1de6691feb499e35
Elio Campitelli <eliocampitelli@gmail.com>
2020-11-13 13:10:44 -0300

    Agrega opcion de usar vector lógico en referencia (deciles y porcentual)

--- R/decil.R
+++ R/decil.R
@@ -2,14 +2,18 @@
 #'
 #'
 #'
-#' @param variable Vector de observaciones de la variable de interés.
-#' @param referencia Vector de observacione de la variable de interés para usar de
-#' referencia al calcular la transformación. Por defecto usa todos los
-#' datos incluidos en el argumento variable.
-#'
+#' @param variable vector de observaciones de la variable de interés.
+#' @param referencia serie de precipitación para usar de referencia en el ajuste
+#' a la distribución teórica. Puede ser:
+#' * vector lógico que se usará para filtrar los datos de entrada.
+#' * vector numérico de observaciones.
 #'
 #' @export
-decil <- function(variable, referencia = variable) {
+decil <- function(variable, referencia = rep(TRUE, length(variable))) {
+  if (is.logical(referencia)) {
+    referencia <- variable[referencia]
+  }
+
   deciles <- stats::ecdf(referencia)(variable)*10
   return(deciles)
 }
@@ -17,7 +21,10 @@ decil <- function(variable, referencia = variable) {
 
 #' @export
 #' @rdname decil
-anomalia_porcentual <- function(variable, referencia = variable) {
+anomalia_porcentual <- function(variable, referencia = rep(TRUE, length(variable))) {
+  if (is.logical(referencia)) {
+    referencia <- variable[referencia]
+  }
   percent <- variable/mean(referencia) - 1
   return(percent)
 }
