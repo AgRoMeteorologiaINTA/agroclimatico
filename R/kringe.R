96c7ca8ac157dadc6bb56bac33329d12156c3a6f
Pao Corrales <paobcorrales@gmail.com>
2023-06-04 19:14:56 -0300

    saca sp como dependencia

--- R/kringe.R
+++ R/kringe.R
@@ -5,14 +5,18 @@ kringe <- function(valor, lon, lat) {
   datos <- data.frame(valor = valor, lon = lon, lat = lat)
   datos <- stats::na.omit(datos)
 
-  sp::coordinates(datos) <- ~ lon + lat
-  sp::coordinates(points) <- ~ lon + lat
+  datos <- sf::st_as_sf(datos, coords = c("lon", "lat"))
+  points <- sf::st_as_sf(points, coords = c("lon", "lat"))
 
 
   k <- automap::autoKrige(valor ~ 1, datos, new_data = points)
 
 
   campo <- as.data.frame(k$krige_output)
+  coords <- sf::st_coordinates(campo$geometry)
+  campo$lon <- coords[,1]
+  campo$lat <- coords[,2]
+
   campo
 
 
